6320744d486bd6770a7fd2231ce50a3ec6cccdb7
nmiguel <nmiguel@my.bcit.ca>
2022-06-05 22:38:48 -0700

    lab4 edit

--- labs/lab4/index.php
+++ labs/lab4/index.php
@@ -1,12 +1,25 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Lab4</title>
 </head>
+
 <body>
-  
+  <div>
+    <form enctype="multipart/form-data" action="server.php" method="POST">
+      <div>
+        <label>Photo</label>
+        <input type="file" name="photo">
+      </div>
+      <div>
+        <input type="submit" value="Upload">
+      </div>
+    </form>
+  </div>
 </body>
+
 </html>
